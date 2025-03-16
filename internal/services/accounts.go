cca6cae9281412f609f6650848926dd7b0817a99
Pedro Santos <p.augustocampos@gmail.com>
2025-03-16 17:13:47 -0300

    feat: accounts service setup

--- internal/services/accounts.go
+++ internal/services/accounts.go
@@ -2,13 +2,29 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/ppcamp/go-pismo-code-challenge/internal/repositories"
 	"github.com/ppcamp/go-pismo-code-challenge/pkg/dtos"
 )
 
-type account struct{}
+type Account interface {
+	CreateAccount(ctx context.Context, req *dtos.CreateAccount) error
+	GetAccount(ctx context.Context, id int64) (*dtos.Account, error)
+}
+
+type account struct {
+	Repo repositories.Account
+}
+
+func NewAccountService() Account { return &account{} }
 
 func (t *account) CreateAccount(ctx context.Context, req *dtos.CreateAccount) error {
+	err := t.Repo.Create()
+	if err != nil {
+		return fmt.Errorf("fail to create account: %w", err)
+	}
+
 	return nil
 }
 
