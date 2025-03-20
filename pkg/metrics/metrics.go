750ed3b386061e17b435905e1ffba68231dfdd3b
Pedro Santos <p.augustocampos@gmail.com>
2025-03-19 21:25:21 -0300

    fix: add missing otel resource

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"sync"
 
+	"github.com/ppcamp/go-pismo-code-challenge/internal/config"
+	"github.com/spf13/viper"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
@@ -12,7 +14,9 @@ import (
 	"go.opentelemetry.io/otel/log/global"
 	"go.opentelemetry.io/otel/sdk/log"
 	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 )
 
 type shutdownFunc func(context.Context) error
@@ -29,15 +33,21 @@ func Init(ctx context.Context) error {
 	var err error
 
 	m.start.Do(func() {
-		if err = m.initLogs(ctx); err != nil {
+		var res *resource.Resource
+		res, err = m.resource(ctx)
+		if err != nil {
 			return
 		}
 
-		if err = m.initTraces(ctx); err != nil {
+		if err = m.initLogs(ctx, res); err != nil {
 			return
 		}
 
-		if err = m.initMetrics(ctx); err != nil {
+		if err = m.initTraces(ctx, res); err != nil {
+			return
+		}
+
+		if err = m.initMetrics(ctx, res); err != nil {
 			return
 		}
 	})
@@ -63,15 +73,27 @@ func Shutdown(ctx context.Context) error {
 	return nil
 }
 
+func (m *management) resource(ctx context.Context) (*resource.Resource, error) {
+	return resource.New(
+		ctx,
+		resource.WithAttributes(
+			semconv.ServiceNameKey.String(viper.GetString(config.AppName)),
+		),
+	)
+}
+
 // https://pkg.go.dev/go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp#example-package
-func (m *management) initLogs(ctx context.Context) error {
+func (m *management) initLogs(ctx context.Context, res *resource.Resource) error {
 	exp, err := otlploghttp.New(ctx)
 	if err != nil {
 		return fmt.Errorf("fail to initialize otel log: %w", err)
 	}
 
 	processor := log.NewBatchProcessor(exp)
-	provider := log.NewLoggerProvider(log.WithProcessor(processor))
+	provider := log.NewLoggerProvider(
+		log.WithProcessor(processor),
+		log.WithResource(res))
+
 	m.shutdownCbs = append(m.shutdownCbs, provider.Shutdown)
 
 	global.SetLoggerProvider(provider)
@@ -79,13 +101,15 @@ func (m *management) initLogs(ctx context.Context) error {
 }
 
 // https://pkg.go.dev/go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp#example-package
-func (m *management) initTraces(ctx context.Context) error {
+func (m *management) initTraces(ctx context.Context, res *resource.Resource) error {
 	exp, err := otlptracehttp.New(ctx)
 	if err != nil {
 		return fmt.Errorf("fail to initialize otel tracers: %w", err)
 	}
 
-	tracerProvider := trace.NewTracerProvider(trace.WithBatcher(exp))
+	tracerProvider := trace.NewTracerProvider(
+		trace.WithBatcher(exp),
+		trace.WithResource(res))
 	m.shutdownCbs = append(m.shutdownCbs, tracerProvider.Shutdown)
 
 	otel.SetTracerProvider(tracerProvider)
@@ -93,13 +117,15 @@ func (m *management) initTraces(ctx context.Context) error {
 }
 
 // https://pkg.go.dev/go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp#example-package
-func (m *management) initMetrics(ctx context.Context) error {
+func (m *management) initMetrics(ctx context.Context, res *resource.Resource) error {
 	exp, err := otlpmetrichttp.New(ctx)
 	if err != nil {
 		panic(err)
 	}
 
-	meterProvider := metric.NewMeterProvider(metric.WithReader(metric.NewPeriodicReader(exp)))
+	meterProvider := metric.NewMeterProvider(
+		metric.WithReader(metric.NewPeriodicReader(exp)),
+		metric.WithResource(res))
 	m.shutdownCbs = append(m.shutdownCbs, meterProvider.Shutdown)
 
 	otel.SetMeterProvider(meterProvider)
